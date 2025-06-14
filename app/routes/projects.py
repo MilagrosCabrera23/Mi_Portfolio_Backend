9db4e7a6c441b384c460d4d422d9f7f5c9b0232f
MilagrosCabrera <maguicabrera23@gmail.com>
2025-06-14 14:22:58 -0300

    Mejoramos proyectos

--- app/routes/projects.py
+++ app/routes/projects.py
@@ -13,7 +13,7 @@ proyectos = [
         imagen="http://127.0.0.1:8000/assets/img/arg-broker.png",
         titulo=" Arg Broker – Gestión de Inversiones",
         descripcion="Plataforma para gestionar inversiones financieras de forma sencilla. Permite registrarse, comprar y vender acciones, y visualizar el rendimiento del portafolio con gráficos e indicadores", 
-        tecnologias="Python, MySql, Patron DAO",
+        tecnologias="Python, MySql, Patron Dao",
         link_github="https://github.com/MilagrosCabrera23/proyecto_integrador_broker" 
     ), 
      Proyecto(
@@ -21,17 +21,17 @@ proyectos = [
         categoria="Desarrollo Web",
         imagen="http://127.0.0.1:8000/assets/img/ecommerce.png",
         titulo="E-commerce Firebase – Trendy Threads",
-        descripcion=" Catálogo de ropa online organizado por categorías, diseñado para  ofrecer una experiencia de compra cómoda y atractiva. Incluye carrito de compras, detalles completos de productos y navegación intuitiva.",
-        tecnologias="React, Firebase, HTML, CSS, BOOSTRAP REACT",
+        descripcion=" Catálogo de ropa online organizado por categorías, diseñado para  ofrecer una experiencia de compra cómoda y atractiva.",
+        tecnologias="React, Firebase, Html, Css, Boostrap React",
         link_github="https://github.com/MilagrosCabrera23/EntregaFinal-React-CabreraMilagros" 
     ), 
     Proyecto(
         id=3,
         categoria="IOT",
         imagen="http://127.0.0.1:8000/assets/img/sistema-iot.png",
-        titulo="Sistema IoT con Node-RED",
-         descripcion="  Dashboard web en tiempo real para visualizar datos de sensores IoT (ESP32), integrados con MySQL. Incluye monitoreo continuo, alertas automáticas ante valores fuera de rango y escalabilidad para agregar nuevos dispositivos.",
-         tecnologias="Esp32, Node-RED, MySql, Mqtt",
+        titulo="Sistema IoT con Node-Red",
+         descripcion="Dashboard web en tiempo real para visualizar datos de sensores IoT (ESP32), integrados con MySQL. Incluye monitoreo continuo, alertas automáticas y escalabilidad para agregar nuevos dispositivos.",
+         tecnologias="Esp32, Node-Red, MySql, Mqtt",
         link_github="https://github.com/MilagrosCabrera23/Escalado-de-plataforma-Iot-con-visualizacion-de-datos-en-node-red"
         ), 
         Proyecto(
@@ -39,15 +39,15 @@ proyectos = [
             categoria="Desarrollo Web",
             imagen="http://127.0.0.1:8000/assets/img/Thinktopia.png",
             titulo="Thintopia - Plataforma de Actividades Academicas",
-            descripcion="Este proyecto consiste en la creación de una plataforma web para gestionar y registrar actividades académicas en instituciones educativas. La plataforma busca mejorar la organización, participación y motivación de los estudiantes mediante herramientas interactivas, gamificación, métricas de desempeño y funcionalidades de gestión para docentes y estudiantes.",
-            tecnologias="Angular, Django, HTML, CSS,BOOSTRAP",
+            descripcion="Este proyecto consiste en la creación de una plataforma web para gestionar y registrar actividades académicas en instituciones educativas.",
+            tecnologias="Angular, Django, Html, Css,Boostrap",
             link_github="https://github.com/Academic-Developers/Thinktopia-Plataforma-de-actividades-Academicas"
         ),
         Proyecto(
             id=5, 
             imagen="http://127.0.0.1:8000/assets/img/QR.png",
             categoria="Desarrollo Web",
-            titulo="Codificador-Decodificador-QR-Python",
+            titulo="Codificador-Decodificador QR",
             descripcion="Este proyecto es un codificador y decodificador de códigos QR desarrollado en Python. Permite generar códigos QR a partir de texto y leer información desde códigos QR existentes.",
             tecnologias="Python, Streamlit, OpenCV, qrcode",
             link_github="https://github.com/MilagrosCabrera23/Codificador-Decodificador-QR-Python"
@@ -56,9 +56,9 @@ proyectos = [
             id=6, 
             imagen="http://127.0.0.1:8000/assets/img/acortador.png",
             categoria="Desarrollo Web",
-            titulo="Acortador_URL_Python",
+            titulo="Acortador URL",
             descripcion="Esta es una aplicación web simple para acortar URLs utilizando Python y Streamlit. Con esta herramienta, los usuarios pueden ingresar una URL larga y generar una versión corta mediante la API de TinyURL",
-            tecnologias="Python,streamlit, Pyshortener",
+            tecnologias="Python,Streamlit, Pyshortener",
             link_github="https://github.com/MilagrosCabrera23/Acortador_URL_Python"
         ), 
         Proyecto(
@@ -67,7 +67,7 @@ proyectos = [
             categoria="IOT",
             titulo="Desarrollo de Prototipos IoT con ESP32",
             descripcion="Este repositorio contiene la implementación de dos proyectos IoT desarrollados como parte del curso de la Tecnicatura Superior en Desarrollo de Software del ISPC. Ambos proyectos utilizan la plataforma ESP32 y el simulador Wokwi.",
-            tecnologias="",
+            tecnologias="MicroPython, ESP32, Wokwi",
             link_github="https://github.com/MilagrosCabrera23/Desarrollo-de-Prototipos-IoT-con-ESP32n-Wokwi--Iot"
         )
 ]
